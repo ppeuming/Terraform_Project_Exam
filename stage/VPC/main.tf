bf75419d005e49ed24ec93e257eeca93799cc393
ppeuming <happy4evernu@gmail.com>
2023-10-11 12:59:37 +0900

    Temote Backend

---
+++ stage/VPC/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    key            = "stage/vpc/terraform.tfstate" // 상태파일 저장 경로
+    bucket         = "myterraform-bucket-state-hwang-t"
+    region         = "ap-northeast-2"
+    profile        = "terraform_user"
+    dynamodb_table = "myTerraform-bucket-lock-hwang-t"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-2"
+  profile = "terraform_user"
+}
